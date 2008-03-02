b03ea43af90b637fef4f522b778897d94a324596
Nils Jonsson <nils@alumni.rice.edu>
2008-03-02 03:43:49 +0000

    Optimize Trapeze::NameExtension#name by caching the Regexp

--- lib/name_extension.rb
+++ lib/name_extension.rb
@@ -5,15 +5,23 @@
 module Trapeze::NameExtension
   
   def name
-    special_method_characters = '!%&*+-./<=>?@[]^|~'
-    pattern = []
-    pattern << "[#{Regexp.escape '.#'}]"
-    pattern << '([\w'
-    pattern << Regexp.escape(special_method_characters)
-    pattern << ']+)'
-    pattern << '>$'
-    regexp = Regexp.new(pattern.join)
-    to_s.match(regexp).captures.first
+    to_s.match(regexp_for_name).captures.first
+  end
+  
+private
+  
+  def regexp_for_name
+    unless @regexp_for_name
+      special_method_characters = '!%&*+-./<=>?@[]^|~'
+      pattern = []
+      pattern << "[#{Regexp.escape '.#'}]"
+      pattern << '([\w'
+      pattern << Regexp.escape(special_method_characters)
+      pattern << ']+)'
+      pattern << '>$'
+      @regexp_for_name = Regexp.new(pattern.join)
+    end
+    @regexp_for_name
   end
   
 end
