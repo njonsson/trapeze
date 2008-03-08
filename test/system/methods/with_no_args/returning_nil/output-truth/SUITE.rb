6389290655a31aa9261255e1da94c4831889c2ae
Nils Jonsson <nils@alumni.rice.edu>
2008-03-08 17:52:40 +0000

    Fix pattern for finding test files

--- test/system/methods/with_no_args/returning_nil/output-truth/SUITE.rb
+++ test/system/methods/with_no_args/returning_nil/output-truth/SUITE.rb
@@ -5,6 +5,6 @@ Dir.glob("#{File.dirname __FILE__}/../input/**/*.rb") do |source_file|
   require File.expand_path(source_file)
 end
 
-Dir.glob("#{File.dirname __FILE__}/**/*test.rb") do |test_file|
+Dir.glob("#{File.dirname __FILE__}/**/*_test.rb") do |test_file|
   require File.expand_path(test_file)
 end
