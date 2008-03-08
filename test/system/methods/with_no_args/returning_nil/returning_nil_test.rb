d0060e2958743f760b6c61cf5fae0866c0da42a0
Nils Jonsson <nils@alumni.rice.edu>
2008-03-08 17:19:56 +0000

    Add beginnings of simplest system test (code defining a single method with no args that returns nil)

---
+++ test/system/methods/with_no_args/returning_nil/returning_nil_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+module Methods
+  
+  module WithNoArgs
+    
+    class ReturningNilTest < Test::Unit::TestCase
+      
+      def test_truth
+        # TODO: Write this test
+      end
+      
+    end
+    
+  end
+  
+end
