c752517bfa4b710b2962fd4f42314ce4c626fcf6
Nils Jonsson <nils@alumni.rice.edu>
2008-04-07 12:46:21 +0000

    Add system test test/system/modules/with_no_methods

---
+++ test/system/modules/with_no_methods/input/foo_module.rb
@@ -0,0 +1,1 @@
+module FooModule; end
---
+++ test/system/modules/with_no_methods/with_no_methods_test.rb
@@ -0,0 +1,27 @@
+require File.expand_path("#{File.dirname __FILE__}/../../../test")
+require File.expand_path("#{File.dirname __FILE__}/../../../../lib/executor")
+require File.expand_path("#{File.dirname __FILE__}/../../../../lib/truncate_extension")
+require 'test/unit'
+require File.expand_path("#{File.dirname __FILE__}/../../../assertion_helpers_extension")
+
+module Trapeze::SystemTest
+  
+  module Modules
+    
+    class WithNoMethods < Test::Unit::TestCase
+      
+      def test_should_generate_expected_output
+        input_path        = "#{File.dirname __FILE__}/input/**/*.rb"
+        output_path       = "#{File.dirname __FILE__}/output"
+        output_truth_path = "#{File.dirname __FILE__}/output_truth"
+        Dir.truncate output_path if File.directory?(output_path)
+        executor = Trapeze::Executor.new(input_path, output_path)
+        executor.execute!
+        assert_dirs_identical output_truth_path, output_path
+      end
+      
+    end
+    
+  end
+  
+end
