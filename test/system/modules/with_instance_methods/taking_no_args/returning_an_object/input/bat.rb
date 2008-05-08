4321408bf55f30ba69f44114d59b9b4bfb9d0ec9
Nils Jonsson <nils@alumni.rice.edu>
2008-05-08 03:44:16 +0000

    Add a system test of module instance method return types that are unfamiliar objects

---
+++ test/system/modules/with_instance_methods/taking_no_args/returning_an_object/input/bat.rb
@@ -0,0 +1,29 @@
+module Bat
+  
+  class Pwop
+    
+    def initialize
+      @my_state1 = :something_else_again1
+      @my_state2 = :something_else_again2
+    end
+    
+  end
+  
+  class Ding
+    
+    def initialize
+      @my_state1 = :something_else_entirely1
+      @my_state2 = :something_else_entirely2
+    end
+    
+  end
+  
+  def pwop
+    Pwop.new
+  end
+  
+  def ding
+    Ding.new
+  end
+  
+end
